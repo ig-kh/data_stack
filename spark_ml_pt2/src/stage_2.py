e0c10cc667d2bd8251c51206155e278cb61c6ce9
IG KH <122811954+ig-kh@users.noreply.github.com>
2025-05-20 00:14:08 +0300

    yadebil3

--- spark_ml_pt2/src/stage_2.py
+++ spark_ml_pt2/src/stage_2.py
@@ -1,7 +1,25 @@
 from pyspark.sql import SparkSession
 from delta.tables import DeltaTable
 from argparse import ArgumentParser
+from pyspark.sql import types as T
 from pyspark.sql import functions as F
+
+@F.udf(returnType=T.IntegerType()) # AGE attribute
+def normalize_age(age_str: str):
+    return int(age_str.replace(' ', '').replace('-', '').replace('_', ''))
+
+@F.udf(returnType=T.LongType()) # ANNUAL_INCOME attribute
+def normalize_money(amount_str: str):
+    return float(amount_str.replace(' ', '').replace('-', '').replace('_', ''))
+
+def YM2M_parse_transform(df, col): # CREDIT_HISTORY_AGE attribute
+    df = df.withColumn(f"{col}_Y", F.regexp_extract_all("col", F.lit(r"(\d+) Years\s*and\s*(\d+) Months"), 1))
+    df = df.withColumn(f"{col}_M", F.regexp_extract_all("col", F.lit(r"(\d+) Years\s*and\s*(\d+) Months"), 2))
+    df = df.withColumn(f"{col}_transformed", F.when(F.col(f"{col}_Y") != "").otherwise(0)*12 + F.when(F.col(f"{col}_M") != "").otherwise(0))
+    return df.drop(col, f"{col}_Y", f"{col}_M")
+
+def reconstruct_by_group(df, damaged_col, reference_col):
+    
 
 if __name__ == "__main__":
 
@@ -21,7 +39,7 @@ if __name__ == "__main__":
 
     bronze_df = spark.read.format("delta").load(args.src)
     
-
+    bronze_df = bronze_df.na.drop(subset=[])
 
     delta_table = DeltaTable.forPath(spark, args.dst)
     spark.sql(f"""
